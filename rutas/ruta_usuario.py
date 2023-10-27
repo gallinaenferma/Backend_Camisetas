b58f77e3eb112baa5498e61e10a2736125e10e22
unknown <jforero367@gmail.com>
2023-10-27 02:08:46 -0500

    CRUD a la tabla usuario

--- rutas/ruta_usuario.py
+++ rutas/ruta_usuario.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Response
 from config_bd.db import conn
 from modelos.modelo_usuario import usuario #tabla
 from esquemas.esquema_usuario import Usuario
 from cryptography.fernet import Fernet # Cifrar contraseña
+from starlette.status import HTTP_204_NO_CONTENT
 
 key=Fernet.generate_key()
 f=Fernet(key)
 
 usuarios = APIRouter()
+@usuarios.get("/")
+def inicio():
+    return "Hola Inicio"
 
 @usuarios.get("/usuarios")
 def hola():
@@ -25,5 +29,23 @@ def registrar_usuario( user : Usuario):
     nuevo_usuario["contraseña"] = f.encrypt(user.contraseña.encode("utf-8"))
     resultado = conn.execute(usuario.insert().values( nuevo_usuario))
     print(resultado)
-    return "hola mundo 2 "
+    return conn.execute(usuario.select().where(usuario.c.numero_documento == resultado.lastrowid)).first()
+
+@usuarios.get("/usuarios/{numero_documento}")
+def busqueda_usuario_pk(numero_documento:int):
+    return  conn.execute(usuario.select().where(usuario.c.numero_documento == numero_documento )).first()
+
+@usuarios.delete("/usuarios/{numero_documento}") 
+def eliminar_usuario(numero_documento:int):
+    resultado =  conn.execute(usuario.delete().where(usuario.c.numero_documento == numero_documento ))
+    return Response(status_code=HTTP_204_NO_CONTENT)
+
+@usuarios.put("/usuarios/{numero_documento}")
+def actualizar_usuario(numero_documento:int , user: Usuario):
+    conn.execute(usuario.update().values(email=user.email,celular=user.celular,contraseña=user.contraseña,
+    direccion=user.direccion).where(usuario.c.numero_documento == numero_documento ))
+    return "datos de usuario actualizados"
+
+
+
 
